c93e127fe3cbc183019792d08a39610d61cb6b8b
Srinivas Prasad <sprasad@nlsinc.com>
2016-01-15 15:51:35 +0530

    Checking in the project

---
+++ hellomaven/src/main/java/hellomaven/HelloMavenMain.java
@@ -0,0 +1,16 @@
+package hellomaven;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class HelloMavenMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
+		logger.info("Hello Maven!");
+
+
+	}
+
+}
